75f28df7c66e7323cd8048e119181e0a071ea034
Calmend <calvin.ndjoh-messina@calmend.org>
2022-11-06 17:27:10 +0100

    premier chargement

---
+++ index.php
@@ -0,0 +1,32 @@
+<HTML>
+	<HEAD>
+		<TITLE>Calmend github Test</TITLE>
+        >
+	</HEAD>
+
+	<BODY>
+
+    <H2>Version 2</H2>
+ 
+
+<?php
+//phpinfo();
+echo "je connais php, c'est pourquoi c'est lui qui affiche ce que vous lisez" ;
+echo "</br>";
+echo "la date du jour est"."  "."<b>".date("d.m.y")."</b>";
+
+echo "<p>";
+$i=1;
+$j=1;
+for($i=1;$i<11;$i++){
+	for($j=1;$j<11;$j++){
+		echo "$i x $j ="." ".$i*$j;
+		echo "</br>";
+	} 
+echo "</br>";
+}
+	
+?>
+<h2> Hummmm Chef je ne connais pas phpinfo()</h2>
+</BODY>
+</HTML>
